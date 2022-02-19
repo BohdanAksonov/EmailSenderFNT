050ad396f115fb89af5f984443bc4fbdaf4a9933
Bohdan Aksonov <aksonovb@gmail.com>
2022-02-19 20:32:57 +0100

    test

--- src/views/EmailSender.vue
+++ src/views/EmailSender.vue
@@ -14,6 +14,9 @@
       <send-button />
     </v-col>
   </v-row>
+  <v-row>
+    <v-btn @click="test()">Test</v-btn>
+  </v-row>
 </template>
 
 <script lang="ts">
@@ -21,6 +24,7 @@ import { defineComponent } from "vue";
 import EmailTemplate from "@/components/EmailSender/EmailTemplate.vue";
 import Receivers from "@/components/EmailSender/Receivers.vue";
 import SendButton from "@/components/EmailSender/SendButton.vue";
+import http from "@/plugins/http";
 
 export default defineComponent({
   name: "EmailSender",
@@ -32,5 +36,22 @@ export default defineComponent({
   },
 
   computed: {},
+
+  methods: {
+    test() {
+      http
+        .get("test", {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
 });
 </script>
